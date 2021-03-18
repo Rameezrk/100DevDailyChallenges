6acf068f48c61f8f6140c2b31b1377e86586f43d
Rameez <ashkhj@gmail.com>
2021-03-18 22:52:31 +0000

    push

---
+++ Codewar Challenges/9CodeWars.js
@@ -0,0 +1,9 @@
+//challenge 1 - Grasshopper - Basic Function Fixer -  https://www.codewars.com/kata/56200d610758762fb0000002
+
+//solution
+
+function addFive(num) {
+  var total = num + 5
+  return total
+}
+//end
