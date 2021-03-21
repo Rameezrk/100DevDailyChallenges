bd2424e8b11a5729685bdc608f0c718c62cdd867
Rameez <ashkhj@gmail.com>
2021-03-21 17:31:08 +0000

    push

---
+++ Codewar Challenges/11CodeWars.js
@@ -0,0 +1,9 @@
+//challenge 1 -  Beginner - Lost Without a Map - https://www.codewars.com/kata/57f781872e3d8ca2a000007e
+
+//solution
+
+function maps(x){
+  return x.map((a) => a*2)
+}
+
+//end - status
