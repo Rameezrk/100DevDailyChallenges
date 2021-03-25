dadbf878a3089829480f14358fb5c510ba1685c4
rameezrk <ashkhj@gmail.com>
2021-03-25 21:25:44 +0000

    14 file push

---
+++ Codewar Challenges/14CodeWars.js
@@ -0,0 +1,15 @@
+//challenge 1 -   Pre-FizzBuzz Workout #1 - https://www.codewars.com/kata/569e09850a8e371ab200000b
+
+//solution
+
+function preFizz(n) {
+
+  let output = []
+
+  for (let i = 1; i <= n; i++) {
+    output.push(i)
+  }
+  return output
+}
+
+//end
