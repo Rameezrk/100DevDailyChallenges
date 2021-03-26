c8b0df82d1341df7c1ab014e064044a2183a293d
rameezrk <ashkhj@gmail.com>
2021-03-26 10:38:01 +0000

    15 file push

---
+++ Codewar Challenges/15CodeWars.js
@@ -0,0 +1,15 @@
+//challenge 1 -   Grasshopper - Personalized Message - https://www.codewars.com/kata/5772da22b89313a4d50012f7
+
+//solution
+
+function greet (name, owner) {
+  // Add code here
+
+  if (name === owner) {
+    return 'Hello boss'
+  } else {
+    return 'Hello guest'
+  }
+}
+
+//end
