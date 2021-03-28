a9e742fed37c524a3433d4298d755db152c70fd3
rameezrk <ashkhj@gmail.com>
2021-03-28 17:14:01 +0100

    16 file

---
+++ Codewar Challenges/16CodeWars.js
@@ -0,0 +1,10 @@
+//challenge 1 -   Function 3 - multiplying two numbers - https://www.codewars.com/kata/523b66342d0c301ae400003b
+
+//solution
+// Write here your multiply-function
+
+function multiply(a,b) {
+  return a*b
+}
+
+//end
