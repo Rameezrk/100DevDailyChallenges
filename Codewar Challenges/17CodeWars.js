24a4294cb51a0a2586021617d0eb4607b103954d
rameezrk <ashkhj@gmail.com>
2021-03-29 13:04:16 +0100

    17 file push

---
+++ Codewar Challenges/17CodeWars.js
@@ -0,0 +1,10 @@
+//challenge 1 -   Grasshopper - Variable Assignment Debug - https://www.codewars.com/kata/5612e743cab69fec6d000077
+
+//solution
+
+var a = "dev"
+var b = "Lab"
+
+var name = a + b
+
+//end
