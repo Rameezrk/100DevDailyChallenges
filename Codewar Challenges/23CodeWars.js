2efb1040af4027e126a44525cb4eec1e2eb6206d
rameezrk <ashkhj@gmail.com>
2021-04-05 00:39:07 +0100

    push

---
+++ Codewar Challenges/23CodeWars.js
@@ -0,0 +1,10 @@
+//challenge 1 -   USD => CNY - https://www.codewars.com/kata/5977618080ef220766000022
+
+//solution
+
+function usdcny(usd) {
+  let conversion = usd * 6.75
+
+  return `${conversion.toFixed(2)} Chinese Yuan`
+}
+//end
