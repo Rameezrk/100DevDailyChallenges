8f4eefa8fdba50ff1e4bef4c7ab4237ee4296e5d
rameezrk <ashkhj@gmail.com>
2021-04-06 15:22:47 +0100

    new file

---
+++ Codewar Challenges/24CodeWars.js
@@ -0,0 +1,10 @@
+//challenge 1 -   Convert to Binary - https://www.codewars.com/kata/59fca81a5712f9fa4700159a
+//solution
+
+function toBinary(n){
+  let convert = n.toString(2);
+
+  return Number(convert)
+}
+
+//end
