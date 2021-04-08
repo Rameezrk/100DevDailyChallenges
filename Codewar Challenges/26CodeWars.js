82416e5e209fc60190b1c5d31532b40cb6fc0221
rameezrk <ashkhj@gmail.com>
2021-04-08 15:42:58 +0100

    new

---
+++ Codewar Challenges/26CodeWars.js
@@ -0,0 +1,18 @@
+//challenge 1 -   Beginner Series #2 Clock - https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a
+
+//solution
+
+function past(h, m, s){
+  //#Happy Coding! ^_^
+  let sConvert = s * 1000
+  let mConvert = m * 60000
+  let hConvert = h * 3600000
+
+  let output = 0
+
+  return output += sConvert + mConvert + hConvert
+
+
+}
+
+//end
