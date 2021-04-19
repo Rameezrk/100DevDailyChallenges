b2438c1878078ca7755343ea48be45ca11249395
rameezrk <ashkhj@gmail.com>
2021-04-19 13:00:23 +0100

    new

---
+++ Codewar Challenges/28CodeWars.js
@@ -0,0 +1,11 @@
+//challenge 1 - Grasshopper - Terminal game move function - https://www.codewars.com/kata/563a631f7cbbc236cf0000c2
+
+//solution
+
+function move (position, roll) {
+  // return the new position
+
+  return position + (roll*2)
+}
+
+//end
