f80e439c766bcbf2dc6bb7320d87aa8c03c70d01
rameezrk <ashkhj@gmail.com>
2021-04-27 09:11:21 +0100

    new

--- Codewar Challenges/28CodeWars.js
+++ Codewar Challenges/28CodeWars.js
@@ -44,4 +44,24 @@ function getPlanetName(id){
   return name;
 }
 
-//end//////////
+//challenge 3 - Expressions Matter - https://www.codewars.com/kata/5ae62fcf252e66d44d00008e
+
+//solution
+
+function expressionMatter(a, b, c) {
+  //do the calcs of the four variations
+
+  let output1 = a * (b + c)
+  let output2 = a*b*c
+  let output3 = a + b * c
+  let output4 = (a+b) * c
+  let output5 = a+b+c
+
+  let finalResult = [output1,output2,output3,output4,output5]
+
+  //try filter function to get the highest value
+
+  return Math.max(...finalResult)
+
+}
+//end
