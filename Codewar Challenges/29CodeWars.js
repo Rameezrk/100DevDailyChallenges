ad7aa481ea4e6c2514f778f7b02f7f1676b99837
rameez <ashkhj@gmail.com>
2021-05-03 13:03:10 +0100

    update challenge 2

--- Codewar Challenges/29CodeWars.js
+++ Codewar Challenges/29CodeWars.js
@@ -13,4 +13,18 @@ function detect(comment) {
 
 }
 
-//end//
+//challenge 2 - The Feast of Many Beasts - https://www.codewars.com/kata/5aa736a455f906981800360d
+
+//solution
+
+function feast(beast, dish) {
+//your function here
+
+  if (beast[0] === dish[0] && beast[beast.length-1] === dish[dish.length-1]) {
+    return true
+  } else {
+    return false
+  }
+}
+
+//end
