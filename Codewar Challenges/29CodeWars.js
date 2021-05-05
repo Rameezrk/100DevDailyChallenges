d4649cd72ab84fde7e879157c05ad1112e12f5bf
rameezrk <ashkhj@gmail.com>
2021-05-05 09:43:59 +0100

    updates

--- Codewar Challenges/29CodeWars.js
+++ Codewar Challenges/29CodeWars.js
@@ -27,4 +27,14 @@ function feast(beast, dish) {
   }
 }
 
+
+//challenge 3 - Convert a Number to a String! - https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+
+//solution
+
+function numberToString(num) {
+  // Return a string of the number here!
+ return num.toString()
+}
+
 //end//
