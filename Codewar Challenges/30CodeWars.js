7bbacc6ae51811b6bf0b2ce489cf72a172443930
rameez <ashkhj@gmail.com>
2021-05-16 20:43:42 +0100

    new

--- Codewar Challenges/30CodeWars.js
+++ Codewar Challenges/30CodeWars.js
@@ -72,4 +72,16 @@ function twoSort(s) {
   return final.join('')
 }
 
+//challenge 6 - Alan Partridge II - Apple Turnover - https://www.codewars.com/kata/580a094553bd9ec5d800007d
+
+//solution
+
+function apple(x){
+ if (Math.pow(x,2) > 1000) {
+   return 'It\'s hotter than the sun!!'
+ } else {
+   return 'Help yourself to a honeycomb Yorkie for the glovebox.'
+ }
+}
+
 //end
