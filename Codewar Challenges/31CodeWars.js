e1f946f8ef045e1c3083907067f1955579498b39
rameez <ashkhj@gmail.com>
2021-05-18 15:11:30 +0100

    new challenge

---
+++ Codewar Challenges/31CodeWars.js
@@ -0,0 +1,12 @@
+//challenge 1 - Remove First and Last Character - https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0
+
+//solution
+
+function removeChar(str){
+ //You got this!
+
+return str.substring(1, str.length-1)
+};
+
+
+//end
