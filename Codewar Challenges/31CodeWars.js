a1ab8ba951c6a48ad6ed171fcc5b39a0bdba7b3a
rameez <ashkhj@gmail.com>
2021-05-19 15:13:01 +0100

    challenge 2

--- Codewar Challenges/31CodeWars.js
+++ Codewar Challenges/31CodeWars.js
@@ -8,5 +8,15 @@ function removeChar(str){
 return str.substring(1, str.length-1)
 };
 
+//challenge 2 - Sum Mixed Array - https://www.codewars.com/kata/57eaeb9578748ff92a000009
+
+//solution
+
+function sumMix(x){
+//   return x.reduce((a,b) => (a+b))
+
+  let numberConvert = x.map(i => parseInt(i))
+  return numberConvert.reduce((a,b) => (a+b))
+}
 
 //end
