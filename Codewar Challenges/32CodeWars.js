e6797d4c4386a31e40f0c9a400af8098d0173f89
rameezrk <ashkhj@gmail.com>
2021-05-25 10:56:51 +0100

    c2

--- Codewar Challenges/32CodeWars.js
+++ Codewar Challenges/32CodeWars.js
@@ -7,4 +7,18 @@ function reverseWords(str){
   let strJoint = strSplit.reverse().join(' ')
   return strJoint
 }
+
+//challenge 2 - Do I get a bonus? - https://www.codewars.com/kata/56f6ad906b88de513f000d96
+
+//solution
+
+function bonusTime(salary, bonus) {
+// your code here
+  if (bonus === true) {
+    return `£${salary *= 10}`
+  } else {
+    return `£${salary}`
+  }
+}
+
 //end
