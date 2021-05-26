bdd93cad06e6a3bb74eb2d68edf6f221486c65a2
rameezrk <ashkhj@gmail.com>
2021-05-26 12:52:17 +0100

    c3

--- Codewar Challenges/32CodeWars.js
+++ Codewar Challenges/32CodeWars.js
@@ -21,4 +21,13 @@ function bonusTime(salary, bonus) {
   }
 }
 
+//challenge 3 - Dollars and Cents - https://www.codewars.com/kata/55902c5eaa8069a5b4000083
+
+//solution
+
+function formatMoney(amount){
+  // your formatting code here
+ return '$' + amount.toFixed(2)
+}
+
 //end
