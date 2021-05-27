6f0ef3d3016a948974ffa2850c82b25a414b4975
rameezrk <ashkhj@gmail.com>
2021-05-27 11:11:24 +0100

    c4

--- Codewar Challenges/32CodeWars.js
+++ Codewar Challenges/32CodeWars.js
@@ -30,4 +30,12 @@ function formatMoney(amount){
  return '$' + amount.toFixed(2)
 }
 
+//challenge 4 - No Loops 2 - You only need one - https://www.codewars.com/kata/57cc40b2f8392dbf2a0003ce
+
+//solution
+
+function check(a,x){
+  return a.includes(x)
+};
+
 //end
