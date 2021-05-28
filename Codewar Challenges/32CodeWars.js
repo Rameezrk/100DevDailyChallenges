75c9a2ae89823b42d91a39687e9d3969add4504d
rameezrk <ashkhj@gmail.com>
2021-05-28 09:53:40 +0100

    c5

--- Codewar Challenges/32CodeWars.js
+++ Codewar Challenges/32CodeWars.js
@@ -38,4 +38,16 @@ function check(a,x){
   return a.includes(x)
 };
 
+//challenge 5 - How many lightsabers do you own? - https://www.codewars.com/kata/51f9d93b4095e0a7200001b8
+
+//solution
+
+function howManyLightsabersDoYouOwn(name) {
+  if (name === 'Zach') {
+    return 18
+  } else {
+    return 0
+  }
+}
+
 //end
