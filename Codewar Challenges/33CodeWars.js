dc3663505fbbc47ad7290854919823cb6d165099
rameez <ashkhj@gmail.com>
2021-06-11 20:07:10 +0100

    c10

--- Codewar Challenges/33CodeWars.js
+++ Codewar Challenges/33CodeWars.js
@@ -122,4 +122,22 @@ function getRealFloor(n) {
   }
 }
 
+//challenge 10 - Opposites Attract - https://www.codewars.com/kata/555086d53eac039a2a000083
+
+//solution
+
+function lovefunc(flower1, flower2){
+  // moment of truth
+
+  if (flower1 % 2 === 0 && flower2 % 2 === 0) {
+    return false
+  } else if (flower1 % 2 === 1 && flower2 % 2 === 1){
+    return false
+  } else {
+    return true
+  }
+
+}
+
+
 //end
